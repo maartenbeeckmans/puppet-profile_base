5f6aff777fffcfa5ecf8cd7599a64d804e5d19cf
Maarten Beeckmans <maartenb@inuits.eu>
2020-08-14 21:46:18 +0200

    remove backports repository management

--- manifests/repositories.pp
+++ manifests/repositories.pp
@@ -13,21 +13,9 @@
 #
 class profile_base::repositories (
   Boolean $epel      = true,
-  Boolean $backports = true,
 )
 {
   if $facts['os']['family'] == 'RedHat' and $epel {
     class { 'epel': }
-  }
-  if $facts['os']['family'] == 'Debian' {
-    class { 'apt': {
-      update => {
-        frequency => 'daily',
-      },
-    if $backports {
-      class { 'apt::backports':
-        pin => 500
-      }
-    }
   }
 }
