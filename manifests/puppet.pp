cc2a470e1e9e9f93dee88237dc1435af39e19f72
Maarten Beeckmans <maartenb@inuits.eu>
2020-10-31 19:12:47 +0100

    add support for srv domain puppetmaster

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -2,15 +2,19 @@
 #
 #
 class profile_base::puppet (
-  Stdlib::Fqdn $puppetmaster,
+  Optional[String] $puppetmaster    = undef,
+  Boolean          $use_srv_records = false,
+  Optional[String] $srv_domain      = undef,
 ) {
   # Do not add class to puppetmaster
   unless $facts['networking']['fqdn'] =~ /^puppet\w*/ {
     class { 'puppet':
-      agent        => true,
-      server       => false,
-      show_diff    => true,
-      puppetmaster => $puppetmaster,
+      agent           => true,
+      server          => false,
+      show_diff       => true,
+      puppetmaster    => $puppetmaster,
+      use_srv_records => $use_srv_records,
+      srv_domain      => $srv_domain,
     }
   }
 }
