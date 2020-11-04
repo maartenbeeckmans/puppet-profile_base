a99351d0c145f7514a61bf1b0c8df6d6dc2bab1e
Maarten Beeckmans <maartenb@inuits.eu>
2020-11-04 12:31:46 +0100

    install unzip and tar packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -7,7 +7,7 @@
 # $default_packages   Packages to install on the target system
 #
 class profile_base::packages (
-  $default_packages = ['vim', 'tree', 'curl', 'httpie', 'wget', 'htop', 'iftop', 'iotop', 'tmux', 'telnet', 'silversearcher-ag', 'dnsutils'],
+  $default_packages = ['vim', 'tree', 'curl', 'httpie', 'wget', 'htop', 'iftop', 'iotop', 'tmux', 'telnet', 'silversearcher-ag', 'dnsutils', 'unzip', 'tar'],
 )
 {
   package { $default_packages:
