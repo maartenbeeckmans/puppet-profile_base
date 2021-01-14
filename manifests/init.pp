69a2e09914aa5dbfebd8cfe3adb7877a7faf45cd
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-14 20:59:37 +0100

    fix merge

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,45 +2,6 @@
 #
 #
 class profile_base (
-<<<<<<< HEAD
-  Boolean           $manage_network,
-  String            $interface,
-  Boolean           $dhcp,
-  String            $ipaddress,
-  String            $netmask,
-  String            $gateway,
-  Optional[String]  $domain,
-  Array             $name_servers,
-  Array[String]     $default_packages,
-  Hash              $users,
-  Hash              $groups,
-  Hash              $sudo_confs,
-  String            $firewall_ensure,
-  Boolean           $firewall_purge,
-  Optional[String]  $motd_message,
-  String            $motd_file,
-  Array             $ntp_servers,
-  Array             $ntp_restrictions,
-  Array             $ntp_restrictions_defaults,
-  Boolean           $manage_firewall_entry,
-  Hash              $mounts,
-  String            $sshd_package_name,
-  String            $sshd_service_name,
-  String            $ssh_port,
-  String            $ssh_permit_root_login,
-  String            $ssh_password_authentication,
-  String            $ssh_print_motd,
-  String            $ssh_x11_forwarding,
-  Boolean           $manage_puppet,
-  Optional[String]  $puppetmaster,
-  Boolean           $puppet_use_srv_records,
-  Optional[String]  $puppet_srv_domain,
-  Integer           $puppet_runs_per_hour,
-  Boolean           $manage_fail2ban,
-  String            $timezone,
-  Array             $fail2ban_services,
-  Boolean           $manage_sd_service,
-=======
   Boolean          $manage_network,
   String           $interface,
   Boolean          $dhcp,
@@ -78,7 +39,6 @@ class profile_base (
   Optional[String] $puppetmaster                = undef,
   Optional[String] $puppet_srv_domain           = undef,
   Optional[String] $postfix_relayhost           = undef,
->>>>>>> e3b53b5 (manage postfix and fix vars)
 ) {
   if $manage_network {
     include profile_base::network
