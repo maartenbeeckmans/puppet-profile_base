f63b451e4fbf86380ceb3f1d1200f66f93a31968
Maarten Beeckmans <maartenb@inuits.eu>
2021-12-14 21:02:20 +0100

    ntp should not be open in firewall

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -3,7 +3,6 @@
 #
 class profile_base::ntp (
   Array   $ntp_servers               = $::profile_base::ntp_servers,
-  Boolean $manage_firewall_entry     = $::profile_base::manage_firewall_entry,
 ) {
   if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '8' {
     class { 'chrony':
@@ -16,14 +15,6 @@ class profile_base::ntp (
 
     class { 'ntp':
       servers  => $ntp_servers,
-    }
-  }
-
-  if $manage_firewall_entry {
-    firewall{'00123 allow ntp':
-      dport  => '123',
-      proto  => 'udp',
-      action => 'accept',
     }
   }
 }
